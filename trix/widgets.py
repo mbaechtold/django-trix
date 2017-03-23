1bb9c826df640005faa789b11140cdffd45d5412
Pablo Vergés <pablo.verges@4teamwork.ch>
2017-03-23 15:18:02 +0100

    Set toolbar param via widget argument
    also uses template to render the widget.

--- trix/widgets.py
+++ trix/widgets.py
@@ -1,10 +1,17 @@
 from __future__ import unicode_literals
 from django import forms
-from django.utils.html import format_html
+from django.template.loader import render_to_string
 from django.utils.safestring import mark_safe
 
 
 class TrixEditor(forms.Textarea):
+
+    def __init__(self,
+                 *args,
+                 params={},
+                 **kwargs):
+        self.params = params.copy()
+        super(TrixEditor, self).__init__(*args, **kwargs)
 
     def render(self, name, value, attrs=None):
 
@@ -12,18 +19,28 @@ class TrixEditor(forms.Textarea):
             attrs = {}
         attrs.update({'style': 'visibility: hidden; position: absolute;'})
 
-        params = {
-            'input': attrs.get('id') or '{}_id'.format(name),
-            'class': 'trix-content',
-        }
-        param_str = ' '.join('{}="{}"'.format(k, v) for k, v in params.items())
+        self.params.update({
+            'input': (self.params.get('input') or
+                      attrs.get('id') or
+                      '{}_id'.format(name)),
+            'class': ' '.join(
+                self.params.get('class', '').split(' ') + ['trix-content']
+            ),
+        })
 
-        html = super(TrixEditor, self).render(name, value, attrs)
-        html = format_html(
-            '{}<p><trix-editor {}></trix-editor></p>',
-            html,
-            mark_safe(param_str))
-        return html
+        param_str = ' '.join('{}="{}"'.format(k, v)
+                             for k, v in self.params.items())
+
+        textarea = super(TrixEditor, self).render(name, value, attrs)
+
+        return render_to_string(
+            'trix/widget.html',
+            context={
+                'toolbar_id': self.params.get('toolbar'),
+                'textarea': textarea,
+                'params': mark_safe(param_str),
+            }
+        )
 
     class Media:
         css = {'all': ('trix/trix.css',)}
